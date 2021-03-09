8fa936f647c0d004ee63184ff1ca394ff41dac0a
Ivo Smid <ivo.smid@gmail.com>
2021-03-09 19:30:00 +0100

    Initial commit

---
+++ src/main/kotlin/cz/bedla/countries/CountryBordersApplication.kt
@@ -0,0 +1,11 @@
+package cz.bedla.countries
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CountryBordersApplication
+
+fun main(args: Array<String>) {
+	runApplication<CountryBordersApplication>(*args)
+}
---
+++ src/test/kotlin/cz/bedla/countries/CountryBordersApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.bedla.countries
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CountryBordersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
